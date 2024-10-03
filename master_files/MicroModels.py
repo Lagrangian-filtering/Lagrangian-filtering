fe4c4e7b28d0be47f3a18403bf7cc988df8b74ec
ThomasCelora <thomas.celora@gmail.com>
2024-10-03 16:55:00 +0200

    Initial werk on iMHD_3D

--- master_files/MicroModels.py
+++ master_files/MicroModels.py
@@ -23,8 +23,8 @@ from system.BaseFunctionality import *
 # levi3D = np.array([[[ np.sign(i-j) * np.sign(j- k) * np.sign(k-i) \
 #                       for k in range(3)]for j in range(3) ] for i in range(3) ])
 
-# levi4D = np.array([[[[ np.sign(i - j) * np.sign(j - k) * np.sign(k - l) * np.sign(i - l) \
-#                        for l in range(4)] for k in range(4) ] for j in range(4)] for i in range(4)])
+levi4D = np.array([[[[ np.sign(i - j) * np.sign(j - k) * np.sign(k - l) * np.sign(i - l) \
+                       for l in range(4)] for k in range(4) ] for j in range(4)] for i in range(4)])
 
 class IdealHD_2D(object):
     """
@@ -248,7 +248,7 @@ class IdealHD_2D(object):
 
 class IdealHD_3D(object):
     """
-    Work in progress: micro_model for 3D hydrodynamics ideal fluid data 
+    Class for storing data of 3D hydrodynamic simulations (of turbulence).  
     """
 
     def __init__(self, interp_method = "linear"):
@@ -525,8 +525,247 @@ class IdealHD_3D(object):
         #         self.structures['BC'][h,i,j,k] = result[1]
         #         self.structures['SET'][h,i,j,k] = result[2]
                 
-class IdealMHD_2D(object):
+class IdealMHD_3D(object):
     """
+    Class for storing data of 3D magneto-hydrodynamic simulations (of turbulence). 
     """
-    pass
+    def __init__(self, interp_method = "linear"):
+        """
+        Sets up the main variables and dictionaries. Var and dict names need not match
+        those of the simulation output data, this can be taken into account using appropriate dictionaries of
+        classes in FileReaders.py 
 
+        Parameters
+        ----------
+        interp_method: str
+            optional method to be used by interpn        
+        """
+        self.spatial_dims = 3
+        self.interp_method = interp_method
+
+        self.metric = np.zeros((4,4))
+        self.metric[0,0] = -1
+        self.metric[1,1] = self.metric[2,2] = self.metric[3,3] = +1
+
+        #Dictionary for grid: info and points
+        self.domain_int_strs = ('nt','nx','ny', 'nz')
+        self.domain_float_strs = ("tmin","tmax","xmin","xmax","ymin","ymax","zmin","zmax","dt","dx","dy", "dz")
+        self.domain_array_strs = ("t","x","y","z","points")
+        self.domain_vars = dict.fromkeys(self.domain_int_strs+self.domain_float_strs+self.domain_array_strs)
+        for str in self.domain_vars:
+            self.domain_vars[str] = []   
+
+        #Dictionary for primitive var
+        self.prim_strs = ("vx","vy","vz","n","p","Bx", "By", "Bz") #Bx etc is measured in the `lab-frame'
+        self.prim_vars = dict.fromkeys(self.prim_strs)
+        for str in self.prim_strs:
+            self.prim_vars[str] = []
+
+        #Dictionary for auxiliary var
+        self.aux_strs = ("W", "h")
+        self.aux_vars = dict.fromkeys(self.aux_strs)
+        for str in self.aux_strs:
+            self.aux_vars[str] = []
+
+        #Dictionary for structures
+        self.structures_strs = ("BC", "SET", "Fab") #Fab is the Faraday tensor (not Maxwell)
+        self.structures = dict.fromkeys(self.structures_strs)
+        for str in self.structures_strs:
+            self.structures[str] = []
+
+        self.labels_var_dict = {'BC' : r'$n^{a}$', 
+                                'SET' : r'$T^{ab}$', 
+                                'Fab' : r'$F^{ab}$',
+                                'vx' : r'$v_x$', 
+                                'vy' : r'$v_y$', 
+                                'vz' : r'$v_z$', 
+                                'n' : r'$n$', 
+                                'p' : r'$p$',
+                                'Bx' : r'$B^x$',
+                                'By' : r'$B^y$',
+                                'Bz' : r'$B^z$',
+                                'W' : r'$W$', 
+                                'h' : r'$h$', 
+                                }     
+
+    def get_spatial_dims(self):
+        return self.spatial_dims
+
+    def get_model_name(self):
+        return 'Ideal MHD (3+1d)'
+
+    def get_domain_strs(self):
+        return self.domain_int_strs + self.domain_float_strs + self.domain_array_strs
+    
+    def get_prim_strs(self):
+        return self.prim_strs
+    
+    def get_aux_strs(self):
+        return self.aux_strs
+    
+    def get_structures_strs(self):
+        return self.structures_strs
+    
+    def get_all_var_strs(self):
+        return self.get_prim_strs() + self.get_aux_strs() + self.get_structures_strs()
+    
+    def get_gridpoints(self): 
+        return self.domain_vars['points']
+
+    def get_interpol_var(self, var, point):
+        """
+        Returns quantity corresponding to input str 'var' evaluated 
+        at the input point 'point' via interpolation.
+
+        Parameters
+        ----------
+        var: str corresponding to primitive, auxiliary or structre variable
+            
+        point : list of floats
+            ordered coordinates: t,x,y,z
+
+        Return
+        ------
+        Interpolated values/arrays corresponding to variable. 
+        Empty list if none of the variables is a primitive, auxiliary o structure of the micro_model
+
+        Notes
+        -----
+        Interpolation may fail when too close to boundary
+        """
+
+        if var in self.get_prim_strs():
+            return interpn(self.domain_vars['points'], self.prim_vars[var], point, method = self.interp_method)[0]
+        elif var in self.get_aux_strs():
+            return interpn(self.domain_vars['points'], self.aux_vars[var], point, method = self.interp_method)[0]
+        elif var in self.get_structures_strs():
+            return interpn(self.domain_vars['points'], self.structures[var], point, method = self.interp_method)[0]
+        else:
+            print(f'{var} is not a primitive, auxiliary variable or structure of the micro_model!!')
+
+    @multimethod
+    def get_var_gridpoint(self, var: str, h: object, i: object, j: object, k: object):
+        """
+        Returns quantity corresponding to input str 'var' evaluated 
+        at the grid-point identified by grid indices h,i,j,k
+
+        Parameters: 
+        -----------
+        var: str
+            String corresponding to primitive, auxiliary or structure variable 
+
+        h,i,j,k: int
+            integers corresponding to position on the grid. 
+
+        Returns: 
+        --------
+        Values or arrays corresponding to variable evaluated at grid-point corresponding to input 'h,i,j,k'. 
+
+        Notes:
+        ------
+        This method is useful e.g. for plotting the raw data. 
+        """
+        if var in self.get_prim_strs():
+            return self.prim_vars[var][h,i,j,k]
+        elif var in self.get_aux_strs():
+            return self.aux_vars[var][h,i,j,k]
+        elif var in self.get_structures_strs():
+            return self.structures[var][h,i,j,k]
+        else: 
+            print('{} is not a variable of model {}'.format(var, self.get_model_name()))
+            return None
+
+    @multimethod
+    def get_var_gridpoint(self, var: str, point: object):
+        """
+        Returns quantity corresponding to input str 'var' evaluated 
+        at at gridpoint closest to input 'point'.
+
+        Parameters:
+        -----------
+        vars: string corresponding to primitive, auxiliary or structure variable
+
+        point: list of 3+1 floats
+
+        Returns: 
+        --------
+        Values or arrays corresponding to variable evaluated at the closest grid-point to input 'point'. 
+
+        Notes:
+        ------
+        This method should be used in case using interpolated values 
+        becomes too expensive. 
+        """
+        indices = Base.find_nearest_cell(point, self.domain_vars['points'])
+        if var in self.get_prim_strs():
+            return self.prim_vars[var][tuple(indices)]  
+        elif var in self.get_aux_strs():
+            return self.aux_vars[var][tuple(indices)]    
+        elif var in self.get_structures_strs():
+            return self.structures[var][tuple(indices)]
+        else: 
+            print(f"{var} is not a variable of the model!")
+            return None
+        
+
+    def setup_structures(self):
+        """
+        Key routine: set up the structures, such as baryon (mass) current BC, Stress-Energy tensor
+        and Faraday tensor. 
+
+        Notes:
+        ------
+        Structures are built as multi-dim np.arrays, with the first (three) indices referring 
+        to the grid, while the last one or two refer to space-time components.
+        
+        The baryon current is stored as a fully contra-variant tensor (index up)
+        The stress-energy tensor is stored as a fully contra-variant tensor (both indices up) 
+        The Faraday tensor is stored as a fully contravariant tensor (both indices up)
+        """
+        self.structures["BC"] = np.zeros((self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny'], 
+                                          self.domain_vars['nz'],4))
+        self.structures["SET"] = np.zeros((self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny'],
+                                           self.domain_vars['nz'],4,4))
+        self.structures["Fab"] = np.zeros((self.domain_vars['nt'],self.domain_vars['nx'],self.domain_vars['ny'],
+                                               self.domain_vars['nz'],4,4))
+
+        for h in range(self.domain_vars['nt']):
+            for i in range(self.domain_vars['nx']):
+                for j in range(self.domain_vars['ny']): 
+                    for k in range(self.domain_vars['nz']): 
+                        vel_vec = np.array([self.aux_vars['W'][h,i,j,k],
+                                        self.aux_vars['W'][h,i,j,k] * self.prim_vars['vx'][h,i,j,k] ,\
+                                        self.aux_vars['W'][h,i,j,k] * self.prim_vars['vy'][h,i,j,k], 
+                                        self.aux_vars['W'][h,i,j,k] * self.prim_vars['vz'][h,i,j,k]])
+
+                        self.structures['BC'][h,i,j,k,:] = np.multiply(self.prim_vars['n'][h,i,j,k], vel_vec )
+
+
+                        Bs = np.array([self.prim_vars['Bx'][h,i,j,k], self.prim_vars['By'][h,i,j,k], self.prim_vars['Bz'][h,i,j,k]])
+
+                        b0 = np.dot(vel_vec[1:], Bs)
+                        bi = np.multiply(1/self.aux_vars['W'][h,i,j,k], Bs) + np.multiply(b0/self.aux_vars['W'][h,i,j,k], vel_vec[1:]) 
+                        ba = np.array([b0] + list(bi))
+                        bsq = Base.Mink_dot(ba, ba)
+                        # bsq = np.multiply( 1/(self.aux_vars['W']**2) , np.dot(Bs, Bs)) + np.multiply( 1/self.aux_vars['W'], np.dot(Bs,vel_vec[1:])) 
+                        
+                        rhohstar = np.multiply( self.prim_vars['n'] , self.aux_strs['h'][h,i,j,k] ) + bsq
+                        pstar = self.prim_vars['p'] + bsq/2 
+
+
+                        self.structures['SET'][h,i,j,k,:,:] = np.multiply(rhohstar , np.outer(vel_vec, vel_vec))  + np.multiply(pstar, self.metric ) \
+                                                                + np.outer(ba, ba)
+                        
+                        Maxwell_contrav = np.outer(vel_vec, ba) - np.outer(ba, vel_vec)
+                        Maxwell_covar = np.einsum('ij,kl,jl->ik', self.metric, self.metric, Maxwell_contrav )
+
+                        self.structures['Fab'][h,i,j,k,:,:] = np.multiply(1/2, np.einsum('ijkl,kl->ij', self.Levi4D, Maxwell_covar) )
+                        
+
+                        
+                    
+                    
+
+        self.vars = self.prim_vars
+        self.vars.update(self.aux_vars)
+        self.vars.update(self.structures)
